6340f18194336321e5d166bb88e24c1dcfa6b10c
Petur jack <peturwj@gmail.com>
2024-08-01 09:13:27 +0000

    adding some cool stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { Sunlogo } from "./styled/container"
-import { Container } from "./styled/container"
-import { MainPage } from "./styled/container"
-import sun1 from "./images/sungod.svg"
-
-
+import { Container, Sunlogo, MainPage } from "./styled/container";
+import sun1 from "./images/sungod.svg";
 
 function App() {
-  return(
+  return (
     <>
-    <MainPage>
-     <Sunlogo drag dragConstraints={{top:0, right: 0, bottom: 0, left: 0,}}  dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }} src= {sun1}></Sunlogo>
-    <Container drag dragConstraints={{top:0, right: 0, bottom: 0, left: 0,}}  whileTap={{scale:0.8, rotate:-360, borderRadius:"100%"}} whileHover={{scale: 1.2, rotate: 45,  transition: {duration: 0.5, ease: "easeInOut"}  }}>
-      <h2>Drag</h2>
-    </Container>
-    </MainPage>
+      <MainPage>
+        <Sunlogo
+          drag
+          dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+          src={sun1}
+        ></Sunlogo>
+        <Container
+          drag
+          dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          whileTap={{ scale: 0.8, rotate: -360, borderRadius: "100%" }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 45,
+            transition: { duration: 0.5, ease: "easeInOut" },
+          }}
+        >
+          <h2>Drag</h2>
+        </Container>
+      </MainPage>
     </>
-  )
+  );
 }
 
-
-export default App
-
-
+export default App;
--- src/styled/container.tsx
+++ src/styled/container.tsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
 
 export const Sunlogo = styled(motion.img)`
-    width: 60px;
-    height: 60px;
-    position: absolute;
-    top: 30px;
-    left: 50px;
-`
-
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  top: 30px;
+  left: 50px;
+`;
 
 export const Container = styled(motion.div)`
-height: 100px;
-width: 100px;
-background-color: white;
-border-radius: 20px;
-cursor: pointer;
-display: flex;
-justify-content: center;
-align-items: center;
-
-`
+  height: 100px;
+  width: 100px;
+  background-color: white;
+  border-radius: 20px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const MainPage = styled.main`
-height: 100dvh;
-background: linear-gradient(180deg, blue, red);
-width: 100%;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  height: 100dvh;
+  background: linear-gradient(180deg, #bb00ff, #ff05f7);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
